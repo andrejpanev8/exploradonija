054d8f69fa36813ac7a4b975a1edcaf8626ab07a
AndrejDev <andrejpanev8@gmail.com>
2025-05-19 22:17:22 +0200

    Initial

---
+++ back_end/src/main/java/com/exploradonija/exploradonija/ExploradonijaApplication.java
@@ -0,0 +1,13 @@
+package com.exploradonija.exploradonija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploradonijaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploradonijaApplication.class, args);
+	}
+
+}
---
+++ back_end/src/test/java/com/exploradonija/exploradonija/ExploradonijaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exploradonija.exploradonija;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploradonijaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
